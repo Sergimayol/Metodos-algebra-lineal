09fc6a916c9e3d891e5c1dc43c52bb0d03481008
Sergi Mayol <83218617+Sergimayol@users.noreply.github.com>
2021-05-19 23:01:05 +0200

    Update PA_LUfact.m

--- FACTORIZACION LU/PA_LUfact.m
+++ FACTORIZACION LU/PA_LUfact.m
@@ -1,5 +1,5 @@
 #FACTORIZACIÓN PA = LU
-function [L,U,p]=PA_LUfact(A) 
+function [L, U, w] = PA_LUFact(A)
 %LUFACT: Factor the maxtrix A into LU up to a permutation.
 %
 %input arguments:
@@ -9,24 +9,49 @@ function [L,U,p]=PA_LUfact(A)
 % L                     A square lower triangular matrix with ones along
 %                       the main diagonal.
 % U                     An upper triangular matrix
-% p                     A permutation vector.
-M = A;
-[m,n] = size(A);
-L = eye(m,m);
-U = zeros(m,n);
-p = (1:m)';
-for i=1:m
-    % pivot the largest element and record the permutation vector
-    [Ma,I]=max(abs(M(i:m,i)));
-    I = I+i-1;
-    M([i I],:) = M([I i],:);
-    p([i I])= p([I i]);
-    % row reduction
-    for j=i+1:m
-        norm = M(j,i)/M(i,i);
-        M(j,:)= M(j,:)-norm*M(i,:);
-        L(j,i)= norm;
-    endfor
-endfor
-U = M;
-endfunction
+% w                     A permutation vector.
+    n = size(A, 1);
+    L = eye(n);
+    w = 1 : n;
+    for k = 1 : n - 1       
+        p = k;
+        for j = k : n
+            if abs(A(p, k)) < abs(A(j, k))
+                p = j;
+            end
+        end
+        
+        if A(p, k) == 0
+            printf("No admite fact LU")
+            return
+        end
+        
+        if p ~= k
+            linP = A(p, :);
+            A(p, :) = A(k, :);
+            A(k, :) = linP;
+            
+            wP = w(p);
+            w(p) = w(k);
+            w(k) = wP;
+            
+            if k > 1
+                subLinP = L(p, 1 : k - 1);
+                L(p, 1 : k - 1) = L(k, 1 : k - 1);
+                L(k, 1 : k - 1) = subLinP;
+            end
+        end
+        
+        for l = k + 1 : n
+            L(l, k) = A(l, k) / A(k, k);
+            A(l, :) = A(l, :) - L(l, k) * A(k, :);
+        end
+    end
+    
+    if A(n, n) == 0
+            printf("No admite fact LU")
+        return;
+    end
+    
+    U = A;
+end
