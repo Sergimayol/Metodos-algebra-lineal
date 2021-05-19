698d1df0cde5a22254aaca889256b94b927576a7
Sergi Mayol <83218617+Sergimayol@users.noreply.github.com>
2021-05-19 19:20:14 +0200

    Add files via upload

---
+++ METODOS GAUSS/GaussClasico.m
@@ -0,0 +1,1 @@
+#MÉTODO DE GAUSS SIMPLEfunction[A,b]=GaussClasico(A,b)  n=length(b);  for j = 1:n-1    for k = j+1:n      al=-A(k,j)/A(j,j);      for m = j:n        A(k,m)=A(k,m)+al*A(j,m);      endfor      b(k)= b(k)+al*b(j);    endfor  endforendfunction
